329f5780f223008f726371ec7234f1a4bfa07722
Francisco Mendoza <frgmendoza@gmail.com>
2025-06-30 10:42:12 -0700

    Refactor TrustBadges component: update image sizes and remove grayscale filter

--- src/components/TrustBadges.tsx
+++ src/components/TrustBadges.tsx
@@ -8,13 +8,13 @@ const TrustBadges = () => {
         <p className={styles.title}>Confianza y Calidad</p>
         <div className={styles.badgesContainer}>
           <div className={styles.badge}>
-            <Image src="/icons/rappi.svg" alt="Rappi" width={80} height={30} />
+            <Image src="/icons/rappi.svg" alt="Rappi" width={50} height={50} />
           </div>
           <div className={styles.badge}>
-            <Image src="/icons/uber_eats.svg" alt="Uber Eats" width={80} height={30} />
+            <Image src="/icons/uber_eats.svg" alt="Uber Eats" width={50} height={50} />
           </div>
           <div className={styles.badge}>
-            <Image src="/icons/didi_food.svg" alt="Didi Food" width={80} height={30} />
+            <Image src="/icons/didi_food.svg" alt="Didi Food" width={50} height={50} />
           </div>
           <div className={styles.badge}>
             <span className={styles.icon}>⭐</span>
